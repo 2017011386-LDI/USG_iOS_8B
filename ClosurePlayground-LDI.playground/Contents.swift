d710a1376026e5bb8911bb028fe7ed3d7b335a96
2017011386-LDI <80309156+2017011386-LDI@users.noreply.github.com>
2021-07-25 15:02:00 +0900

    LDI added function type playground

---
+++ ClosurePlayground-LDI.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+func func1(_: Int, _: Double) -> Int {
+    return 0
+}
+
+func func2(_: [String]) -> Int {
+    return 1
+}
+
+func func3(_: String, _: Int) -> [String: Int] {
+    return ["": 0]
+}
+
